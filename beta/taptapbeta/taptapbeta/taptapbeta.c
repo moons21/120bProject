6b93f04d1b44676369d7d33a85301f283f94ebc3
Andrew M <21moons21@gmail.com>
2018-06-05 02:25:26 -0700

    Starting to branch for first project demo

---
+++ beta/taptapbeta/taptapbeta/taptapbeta.c
@@ -0,0 +1,18 @@
+/*
+ * taptapbeta.c
+ *
+ * Created: 6/5/2018 2:23:29 AM
+ * Author : moons XPS
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
